c4ca5a7e48939d72abd6e2de60716ba929485d8c
tnsb5257 <148628033+tnsb5257@users.noreply.github.com>
2026-07-10 19:42:19 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        curr = []
+        l = len(nums)
+        def findComb(i,curr,target):
+            if i == l:
+                if target == 0:
+                    res.append(curr.copy())
+                return
+            if nums[i] <= target:
+                curr.append(nums[i])
+                findComb(i,curr,target-nums[i])
+                curr.pop()
+            findComb(i+1,curr,target)
+
+        findComb(0,curr,target)
+        return res
