efd281b915c689b2891c373801c21e59022beff0
tnsb5257 <148628033+tnsb5257@users.noreply.github.com>
2026-08-03 20:39:20 +0530

    Add: valid-tree - submission-11

---
+++ Data Structures & Algorithms/valid-tree/submission-11.py
@@ -0,0 +1,26 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n-1:
+            return False
+        q = collections.deque()
+        neighbours = collections.defaultdict(list)
+        for x,y in edges:
+            neighbours[x].append(y)
+            neighbours[y].append(x)
+
+        for i in neighbours[0]:
+            q.append((i,0))
+        visited = [0]*n
+        visited[0]=1
+        while q:
+            node,parent = q.popleft()
+            visited[node]=1
+            for i in neighbours[node]:
+                if i == parent:
+                    continue
+                if visited[i] and i != parent:
+                    return False
+                if not visited[i]:
+                    q.append((i,node))
+
+        return True if sum(visited) == n else False
