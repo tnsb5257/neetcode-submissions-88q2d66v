97938d4d2d050cecd61955ed10e3bb5342ec6e77
tnsb5257 <148628033+tnsb5257@users.noreply.github.com>
2026-08-05 13:16:56 +0530

    Add: count-connected-components - submission-8

---
+++ Data Structures & Algorithms/count-connected-components/submission-8.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        parent = [i for i in range(n)]
+        rnk = [1]*n
+        ans = n
+
+        def parnt(a):
+            if parent[a]==a:
+                return a
+            temp = a
+            while parent[temp] != temp:
+                temp = parent[temp]
+            parent[a] = temp
+            return temp
+
+        def union(a,b,ans):
+            par_a,par_b = parnt(a),parnt(b)
+            if par_a == par_b:
+                return False
+            if rnk[par_a] > rnk[par_b]:
+                parent[par_b]=par_a
+                rnk[par_a]+=rnk[par_b]
+            else:
+                parent[par_a]=par_b
+                rnk[par_b]+=rnk[par_a]
+            return True
+
+        for x,y in edges:
+            components_changed=union(x,y,ans)
+            ans = ans - 1 if components_changed else ans
+
+        return ans
