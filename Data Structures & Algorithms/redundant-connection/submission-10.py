79280c9c4362144e3d5c299bd40b4318faedb98f
tnsb5257 <148628033+tnsb5257@users.noreply.github.com>
2026-08-08 20:32:48 +0530

    Add: redundant-connection - submission-10

---
+++ Data Structures & Algorithms/redundant-connection/submission-10.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        l = len(edges)
+        parnt = [i for i in range(l+1)]
+        rnk = [1]*(l+1)
+
+        def parent(g):
+            temp = g
+            while parnt[temp] != temp:
+                temp = parnt[temp]
+            parnt[g] = temp
+            return temp
+
+        def addtograph(a,b):
+            par_a,par_b=parent(a),parent(b)
+            if par_a == par_b:
+                return False
+            if rnk[par_a]>=rnk[par_b]:
+                parnt[b]=par_a
+                parnt[par_b]=par_a
+                rnk[par_a]+=1
+            else:
+                parnt[a]=par_b
+                parnt[par_a]=par_b
+                rnk[par_b]+=1
+            return True
+
+        for x,y in edges:
+            if not addtograph(x,y):
+                return [x,y]
+
+        return edges[-1]
